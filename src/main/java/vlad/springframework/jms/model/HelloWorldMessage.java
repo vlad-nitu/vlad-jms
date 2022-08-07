ca87b1bce7f649bed88d9d5d4b0ef03b829c1796
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-08-08 00:37:23 +0300

    Added Java Message Object

---
+++ src/main/java/vlad/springframework/jms/model/HelloWorldMessage.java
@@ -0,0 +1,19 @@
+package vlad.springframework.jms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+    static final long serialVersionUID = 42L;
+    private UUID id;
+    private String message;
+}
