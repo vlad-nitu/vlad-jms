6738ead3016e780d8f2f4dfd5cf512579586d26b
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-08-08 01:17:50 +0300

    Send JMS Messages

--- src/main/java/vlad/springframework/jms/sender/HelloWorldSender.java
+++ src/main/java/vlad/springframework/jms/sender/HelloWorldSender.java
@@ -1,4 +1,32 @@
 package vlad.springframework.jms.sender;
 
-public class HelloSender {
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import vlad.springframework.jms.config.JmsConfig;
+import vlad.springframework.jms.model.HelloWorldMessage;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class HelloWorldSender {
+
+    private final JmsTemplate jmsTemplate;
+
+
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage() {
+        System.out.println("I'm sending a message");
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello World!")
+                .build();
+
+                jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
+
+        System.out.println("Message sent!");
+    }
 }
